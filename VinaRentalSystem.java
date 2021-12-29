a8774300ae6b21d2333e67a3bf88c5f6f1a39bc6
johnkien23 <53600922+johnkien23@users.noreply.github.com>
2021-12-29 23:07:47 +0700

    Update VinaRentalSystem.java

--- VinaRentalSystem.java
+++ VinaRentalSystem.java
@@ -31,9 +31,41 @@ public class VinaRentalSystem{
         rental = new Rental(number,status,group,cus,c);
         rentalList.add(rental);
     }
-    public void addCar(String number, String model, String status, String name ) throws Exception{
-        Car car =(Car) Helper.search(carList,number);
-        car = new Car(number,model,status,name);
+    public void addBranch(String address, int id) throws Exception{
+        Branch branch=(Branch) Helper.search(branchList,id);
+        boolean preb = (branch == null);
+        if(!preb){
+            String mess =" The branch is already in the system";
+            System.out.println(mess);
+            throw new Exception(mess);
+        }
+        
+        branch = new Branch(address, id);
+        branchList.add(branch);
+    }
+
+public void addModel(String name, String type, int number, int numdoor, int consumption) throws Exception{
+        Model model=(Customer) Helper.search(modelList,number);
+        boolean prem = (model == null);
+        if(!prem){
+            String mess =" The model is already in the system";
+            System.out.println(mess);
+            throw new Exception(mess);
+        }
+        
+        model = new Model(name,type, number, numdoor, consumption);
+        modelList.add(model);
+    }
+
+public void addCar(int number, String model, String status, String name) throws Exception{
+        Car car=(Car) Helper.search(carList,number);
+        boolean prec = (car == null);
+        if(!prec){
+            String mess =" The car is already in the system";
+            System.out.println(mess);
+            throw new Exception(mess);
+        }
+        
+        car = new Car(number, model, status, name);
         carList.add(car);
     }
-}
